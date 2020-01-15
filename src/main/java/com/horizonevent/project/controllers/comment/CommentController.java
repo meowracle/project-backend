79d4ab5a037c34469361b76831fd99a9a36cf235
into-theDarkness <zzpeterpan0510@gmail.com>
2020-01-15 10:40:10 +0700

    find-by-Id

--- src/main/java/com/horizonevent/project/controllers/comment/CommentController.java
+++ src/main/java/com/horizonevent/project/controllers/comment/CommentController.java
@@ -5,11 +5,14 @@ import com.horizonevent.project.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(maxAge = 3600)
 @RestController
@@ -25,12 +28,22 @@ public class CommentController {
         return new ResponseEntity<List<Comment>>(comments, HttpStatus.OK);
     }
     @RequestMapping(value = "/api/comments", method = RequestMethod.POST)
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<Void> createComment(@RequestBody Comment comment, UriComponentsBuilder uriComponentsBuilder){
         System.out.println("Creating Comments" + comment.getDescription());
         commentService.save(comment);
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(uriComponentsBuilder.path("/comments/{id}").buildAndExpand(comment.getId()).toUri());
         return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+    }
+    @RequestMapping(value = "/api/comments/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Optional<Comment>> getComments(@PathVariable("id") long id){
+        Optional<Comment> comment = commentService.findById(id);
+        if(comment == null){
+            System.out.printf("Comment with id" + id + "not found");
+            return new ResponseEntity<Optional<Comment>>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Optional<Comment>>(comment, HttpStatus.OK);
     }
 }
 
